2f602078ff92482107a779a4f28462cafc71526c
Gabriel URAZA GARCIA <137444684+Gabriel-Uraza611@users.noreply.github.com>
2025-10-18 11:03:28 -0500

    UNIFICACION
    se unifico la clase goal y enemy en entity porque era redundante tener dos clases exactamente iguales

---
+++ src/entity.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_path, bounds):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (90, 110))  # tamaño ajustable
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.bounds = bounds
+
+    def resize_to_cell(self, cell_size, padding=10):
+        """Ajusta el sprite al tamaño de la celda."""
+        size = cell_size - padding
+        self.image = pygame.transform.scale(self.image, (size, size))
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
+
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
